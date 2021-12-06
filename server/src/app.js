eab06787fef73b215645ef018a2272368d5ee4b4
hiroyuki kikuchi <hiroyuki.kikuchi@zozo.com>
2021-12-06 13:52:55 +0900

    fix: cache-control

--- server/src/app.js
+++ server/src/app.js
@@ -22,7 +22,7 @@ app.use(bodyParser.raw({ limit: '10mb' }));
 
 app.use((_req, res, next) => {
   res.header({
-    'Cache-Control': 'max-age=0, no-transform',
+    'Cache-Control': 'max-age=86400000',
     Connection: 'close',
   });
   return next();
