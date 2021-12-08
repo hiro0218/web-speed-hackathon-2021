69ea0c6d16f521124d52d322c74005980a29916e
hiroyuki kikuchi <hiroyuki.kikuchi@zozo.com>
2021-12-08 09:43:55 +0900

    remove: 不要な処理を削除

--- client/src/components/foundation/CoveredImage/CoveredImage.jsx
+++ client/src/components/foundation/CoveredImage/CoveredImage.jsx
@@ -2,9 +2,6 @@ import classNames from 'classnames';
 import sizeOf from 'image-size';
 import { useMemo, useState, useCallback } from 'react';
 
-import { useFetch } from '../../../hooks/use_fetch';
-import { fetchBinary } from '../../../utils/fetchers';
-
 /**
  * @typedef {object} Props
  * @property {string} alt
@@ -16,43 +13,12 @@ import { fetchBinary } from '../../../utils/fetchers';
  * @type {React.VFC<Props>}
  */
 const CoveredImage = ({ alt, src }) => {
-  const { data, isLoading } = useFetch(src, fetchBinary);
-
-  const imageSize = useMemo(() => {
-    return data !== null ? sizeOf(Buffer.from(data)) : null;
-  }, [data]);
-
-  const blobUrl = useMemo(() => {
-    return data !== null ? URL.createObjectURL(new Blob([data])) : null;
-  }, [data]);
-
-  const [containerSize, setContainerSize] = useState({ height: 0, width: 0 });
-  /** @type {React.RefCallback<HTMLDivElement>} */
-  const callbackRef = useCallback((el) => {
-    setContainerSize({
-      height: el?.clientHeight ?? 0,
-      width: el?.clientWidth ?? 0,
-    });
-  }, []);
-
-  if (isLoading || data === null || blobUrl === null) {
-    return null;
-  }
-
-  const containerRatio = containerSize.height / containerSize.width;
-  const imageRatio = imageSize?.height / imageSize?.width;
-
   return (
-    <div ref={callbackRef} className="relative w-full h-full overflow-hidden">
+    <div className="relative w-full h-full overflow-hidden">
       <img
         alt={alt}
-        className={classNames('absolute left-1/2 top-1/2 max-w-none transform -translate-x-1/2 -translate-y-1/2', {
-          'w-auto h-full': containerRatio > imageRatio,
-          'w-full h-auto': containerRatio <= imageRatio,
-        })}
-        src={blobUrl}
-        width={containerSize.width}
-        height={containerSize.height}
+        className="relative w-full h-full overflow-hidden object-cover"
+        src={src}
         loading="lazy"
       />
     </div>
